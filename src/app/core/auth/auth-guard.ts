0a5e9727e2ececb99db8b4d645cec27fa7ebe10a
Vikum Kanishka <vikumkanishka14@gmail.com>
2026-04-08 12:11:38 +0530

    feat(auth): implement authGuard for route protection and role-based navigation feat(auth): create Models component for future enhancements refactor(auth): remove unused token and auth service files and their tests

--- src/app/core/auth/auth-guard.ts
+++ src/app/core/auth/auth-guard.ts
@@ -2,10 +2,10 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 
 export const authGuard: CanActivateFn = (route, state) => {
-
   const router = inject(Router);
   const token = localStorage.getItem("token");
   const userRole = localStorage.getItem("role");
+  const allowedRoles = route.data['roles'];
 
   // If navigating to login and already logged in, redirect based on role
   if (state.url === '/login' || state.url === '/') {
@@ -23,6 +23,11 @@ export const authGuard: CanActivateFn = (route, state) => {
 
   // For all other guarded routes, require token
   if (token) {
+    // Check if the user has an allowed role
+    if (allowedRoles && !allowedRoles.includes(userRole)) {
+      router.navigate(['/unauthorized']);
+      return false;
+    }
     return true;
   }
 
