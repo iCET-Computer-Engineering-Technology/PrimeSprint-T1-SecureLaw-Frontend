bb7379cc0f8157871c4d9699e828162d2cd428e2
Deshan Rashmika <deshanrashmika002@gmail.com>
2026-04-09 22:25:36 +0530

    feat(chat): implement drag-and-drop file upload and enhance attachment handling

--- src/app/core/core-module.ts
+++ src/app/core/core-module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DropZoneDirective } from '../pages/chat/DropZoneDirective';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    DropZoneDirective
+  ],
+  exports: [
+    DropZoneDirective
   ]
 })
 export class CoreModule { }
