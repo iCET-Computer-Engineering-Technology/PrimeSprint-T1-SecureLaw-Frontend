307ec3be138bd9fc1fe49a5a2c6a0728a1d09657
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-04-12 11:32:38 +0530

    Remove unused code and dependencies from forgot password component

--- src/app/auth/login/forgotpassword/forgotpassword.ts
+++ src/app/auth/login/forgotpassword/forgotpassword.ts
@@ -3,13 +3,11 @@ import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
-import { UserService } from '../../../services/user.service';
 
 interface AuthDto {
   email: string;
   newPassword: string;
 }
-
 @Component({
   selector: 'app-forgotpassword',
   templateUrl: './forgotpassword.html',
@@ -19,29 +17,5 @@ interface AuthDto {
 }) 
 
 export class Forgotpassword {
- 
-  private baseUrl = 'http://localhost:8080/api/auth';
-
-  constructor(private http: HttpClient) {}  
-
-  authDto: AuthDto = {
-    email: '',
-    newPassword: ''
-  };
-
-  forgotPassword() {
-    this.http.post(`${this.baseUrl}/forgot-password`, this.authDto).subscribe(
-      response => {
-        console.log('Password reset successful', response);
-        alert('Password reset successful. Please check your email for further instructions.');
-      },
-      error => {
-        console.error('Error resetting password', error);
-        alert('Error resetting password. Please try again later.');
-      }
-    );  
-
-  }
- 
  
 }
